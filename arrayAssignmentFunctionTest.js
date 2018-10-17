22615be7e0b236e9184c1bf4d8948cf155463194
swapnillothe <44019530+swapnillothe@users.noreply.github.com>
2018-10-17 10:21:22 +0530

    one reverse array test is needed to add

--- arrayAssignmentFunctionTest.js
+++ arrayAssignmentFunctionTest.js
@@ -1,26 +1,32 @@
 const assert = require("assert");
 const lib = require("./arrayAssignmentFunction.js");
-
-const {findEvenNumbers,findOddNumbers} = lib;
+const {giveEveryNthElement,findEvenNumbers,findOddNumbers,calculateSum,reverseArray} = lib;
 
 //---------for empty string([])------------//
-givenNumber = [];
+givenNumber = "";
 assert.deepEqual(findOddNumbers(givenNumber),[]);
 assert.deepEqual(findEvenNumbers(givenNumber),[]);
+assert.deepEqual(calculateSum(givenNumber.split(",")),0);
+assert.deepEqual(reverseArray(givenNumber))
 
 //---------for two even numbers string([2,4])------------//
-givenNumber = [2,4];
+givenNumber = "2,4".split(",");
 assert.deepEqual(findOddNumbers(givenNumber),[]);
 assert.deepEqual(findEvenNumbers(givenNumber),[2,4]);
+assert.deepEqual(calculateSum(givenNumber),6);
+
 
 //---------for two odd numbers string([1,3])------------//
-givenNumber = [1,3];
+givenNumber = "1,3".split(",");
 assert.deepEqual(findOddNumbers(givenNumber),[1,3]);
 assert.deepEqual(findEvenNumbers(givenNumber),[]);
+assert.deepEqual(calculateSum(givenNumber),4);
 
 //---------for mix numbers string([1,2,3,4,5,6,7])------------//
-givenNumber = [1,2,3,4,5,6,7];
+givenNumber = "1,2,3,4,5,6,7".split(",");
 assert.deepEqual(findOddNumbers(givenNumber),[1,3,5,7]);
 assert.deepEqual(findEvenNumbers(givenNumber),[2,4,6]);
+assert.deepEqual(calculateSum(givenNumber),28);
+
 
 console.log("-------------Everything is fine-----------------");
